e9a98bb14e1deafcd487df176953ad36ee2b1c60
wiiliam <wiiliamchung360@gmail.com>
2016-04-04 20:40:56 -0700

    (:

--- V1.2/src/bg/background.js
+++ V1.2/src/bg/background.js
@@ -1,20 +1,3 @@
-// if you checked "fancy-settings", uncomment this lines
-
-// var settings = new Store("settings", {
-//     "sample_setting": "This is how you use Store.js to remember values"
-// });
-
-/**
-
-//example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
-
-**/
-
 // ### Rename the page title ###
 var Title = document.title
 var nTitle = Title + " " + "in my butt"
